0c5d1e2c6793217b0cc8a94f7d2bfb667bcbda4e
tobiasberei <bereitobias@hotmail.com>
2019-10-30 13:10:48 +0100

    updated tsconfig to allow decorators

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
-import { AspCore2ILFGrammarVisitor } from './grammar/parser/AspCore2ILFGrammarVisitor';
-import { ParseTreeVisitor } from 'antlr4ts/tree/ParseTreeVisitor';
-import { AspCore2ILFGrammarParser } from './grammar/parser/AspCore2ILFGrammarParser';
-import { AspCore2ILFGrammarLexer } from './grammar/parser/AspCore2ILFGrammarLexer';
 import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
+import { AspCore2ILFGrammarLexer } from './grammar/parser/AspCore2ILFGrammarLexer';
+import { AspCore2ILFGrammarParser } from './grammar/parser/AspCore2ILFGrammarParser';
 
 export const Greeter = (name: string) => `Hello ${name}`;
 
+export class AspCore2ILFParser {
+  public parse(code: string): void {
+    const inputStream = new ANTLRInputStream(code);
+    const lexer = new AspCore2ILFGrammarLexer(inputStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new AspCore2ILFGrammarParser(tokenStream);
 
-export class AspCore2ILFParser{
-
-    public parse(code: string) : void {
-        const inputStream = new ANTLRInputStream(code);
-        const lexer = new AspCore2ILFGrammarLexer(inputStream);
-        const tokenStream = new CommonTokenStream(lexer);
-        const parser = new AspCore2ILFGrammarParser(tokenStream);
-
-        const tree = parser.aspCore2ILFGrammar();
-    }
+    const tree = parser.aspCore2ILFGrammar();
+  }
 }
